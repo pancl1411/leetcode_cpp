0fe5fb67522130e8c9c4cac3108bf01a61fdb211
pancl1411 <1004907659@qq.com>
2020-12-11 01:29:49 +0800

    [103] Binary Tree Zigzag Level Order Traversal

--- 103.binary-tree-zigzag-level-order-traversal.cpp
+++ 103.binary-tree-zigzag-level-order-traversal.cpp
@@ -16,11 +16,58 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+#include <vector>
+#include <list>
+class Solution
+{
 public:
-    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-        
+    // BFS with flag to read from front/back
+    std::vector<std::vector<int>> zigzagLevelOrder(TreeNode *root)
+    {
+        std::vector<std::vector<int>> answer;
+        if (root == nullptr)
+            return answer;
+        // init
+        std::list<TreeNode *> level_nodes;
+        level_nodes.push_back(root);
+        bool is_left_to_right = true;
+        // BFS with flag
+        while (!level_nodes.empty())
+        {
+            std::vector<int> level_values;
+            TreeNode *node = nullptr;
+            int level_size = level_nodes.size();
+
+            for (int i = 0; i < level_size; i++)
+            {
+                // left to right, 
+                if (is_left_to_right)
+                {
+                    node = level_nodes.front();
+                    level_nodes.pop_front();
+                    if (node->left)
+                        level_nodes.push_back(node->left);
+                    if (node->right)
+                        level_nodes.push_back(node->right);
+                }
+                // right to left
+                else
+                {
+                    node = level_nodes.back();
+                    level_nodes.pop_back();
+                    if (node->right)
+                        level_nodes.push_front(node->right);
+                    if (node->left)
+                        level_nodes.push_front(node->left);
+                }
+                //
+                level_values.push_back(node->val);
+            }
+            // record this level values, then reverse flag
+            answer.push_back(level_values);
+            is_left_to_right = !is_left_to_right;
+        }
+        return answer;
     }
 };
 // @lc code=end
-
