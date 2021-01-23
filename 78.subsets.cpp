55c2a3dd53e011c7345a489447481b3dfc601274
pancl1411 <1004907659@qq.com>
2021-01-23 17:57:50 +0800

    [78] Subsets

--- 78.subsets.cpp
+++ 78.subsets.cpp
@@ -5,10 +5,35 @@
  */
 
 // @lc code=start
+#include <vector>
+
 class Solution {
 public:
-    vector<vector<int>> subsets(vector<int>& nums) {
-        
+    std::vector<std::vector<int>> subsets(std::vector<int>& nums) {
+        std::vector<int> set;
+        //_answer.push_back(set); // push empty set
+        subsetsBPImpl(nums, 0, set);
+        return _answer;        
+    }
+private:
+    std::vector<std::vector<int>> _answer;
+    void subsetsBPImpl(std::vector<int>& nums, int start, std::vector<int>& set)
+    {
+        //
+        _answer.push_back(set);
+        //
+        if (start >= nums.size())
+        {
+            return;
+        }
+        //
+        for (int i = start; i < nums.size(); i++)
+        {
+            set.push_back(nums[i]);
+            subsetsBPImpl(nums, i+1, set);
+            set.pop_back();
+        }
+
     }
 };
 // @lc code=end
