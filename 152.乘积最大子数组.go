8c8af1a4fca0e1613de84dc988c6a236d919be66
pancl1411 <1004907659@qq.com>
2021-04-11 19:25:22 +0800

    [152] 乘积最大子数组

---
+++ 152.乘积最大子数组.go
@@ -0,0 +1,45 @@
+/*
+ * @lc app=leetcode.cn id=152 lang=golang
+ *
+ * [152] 乘积最大子数组
+ */
+
+// @lc code=start
+func maxProduct(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+
+	var p, n, result int = max(0, nums[0]), min(0, nums[0]), nums[0]
+	// 注意边界条件i
+	for i := 1; i < len(nums); i++ {
+		if nums[i] == 0 {
+			p = 0
+			n = 0
+		} else if nums[i] < 0 {
+			p, n = max(0, nums[i]*n), min(nums[i], nums[i]*p)
+		} else if nums[i] > 0 {
+			p, n = max(nums[i], nums[i]*p), min(0, nums[i]*n)
+		}
+		result = max(result, p)
+	}
+	//
+	return result
+}
+
+func min(first, second int) int {
+	if first < second {
+		return first
+	}
+	return second
+}
+
+func max(first, second int) int {
+	if first > second {
+		return first
+	}
+	return second
+}
+
+// @lc code=end
+
