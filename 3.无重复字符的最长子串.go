8f4a1b2cced2c75036e0031bee93c7e5f5f32306
pancl1411 <1004907659@qq.com>
2021-04-20 22:37:18 +0800

    [3] 无重复字符的最长子串

---
+++ 3.无重复字符的最长子串.go
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=3 lang=golang
+ *
+ * [3] 无重复字符的最长子串
+ */
+
+// @lc code=start
+func lengthOfLongestSubstring(s string) int {
+	has := make(map[byte]int)
+	var start, maxLen int
+	// 滑动窗口
+	for end := 0; end < len(s); end++ {
+		if idx, ok := has[s[end]]; ok {
+			start = max(start, idx+1)
+		}
+		has[s[end]] = end
+		maxLen = max(maxLen, end-start+1)
+	}
+	//
+	return maxLen
+}
+
+func max(f, s int) int {
+	if f > s {
+		return f
+	}
+	return s
+}
+
+// @lc code=end
+
