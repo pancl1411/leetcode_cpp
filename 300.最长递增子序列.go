5992a23dce2d23437b22f03bb52b59fe34c68423
pancl1411 <1004907659@qq.com>
2021-05-16 00:53:19 +0800

    [300] 最长递增子序列

--- 300.最长递增子序列.go
+++ 300.最长递增子序列.go
@@ -6,7 +6,30 @@
 
 // @lc code=start
 func lengthOfLIS(nums []int) int {
-
+	if len(nums) == 0 {
+		return 0
+	}
+	//
+	var length int = 0
+	dp := make([]int, len(nums))
+	for i := 0; i < len(dp); i++ {
+		dp[i] = 1
+		for j := 0; j < i; j++ {
+			if nums[j] < nums[i] {
+				dp[i] = max(dp[i], dp[j]+1)
+			}
+		}
+		length = max(length, dp[i])
+	}
+	//
+	return length
 }
+func max(i, j int) int {
+	if i > j {
+		return i
+	}
+	return j
+}
+
 // @lc code=end
 
